72e1b295e8aa5f9a14949fb383578c09bd436543
Ovidiu1012 <ovidiu.1012@gmail.com>
2022-11-07 20:09:45 +0200

    Fisrt commit

---
+++ Primii_pasi_in_programare/Tema_Python_Primii_pasi_in_programare.py
@@ -0,0 +1,16 @@
+# 2. Variabila string = spatiu rezervat in memorie pentru valori de tip sir de caractere.
+
+# 4. Declarare si initializare variabile
+# Variabila String
+model_telefon = 'Samsung'
+# Variabila integer
+an_fabricatie = 2021
+# Variabila float
+pret_telefon = 4500.1234
+# Variabila boolean
+in_stoc = True
+
+# 6. Cod exercitiu 5 in limbaj programare Python
+
+for i in range(1,20,3):
+    print(i)
